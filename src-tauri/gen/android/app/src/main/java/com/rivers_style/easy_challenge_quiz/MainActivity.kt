36785d6a50a0ce6d7f4c6e28ae9511b4d20adf06
kawaji-r <kawaji.r.333@gmail.com>
2026-03-01 16:59:44 +0900

    android init

---
+++ src-tauri/gen/android/app/src/main/java/com/rivers_style/easy_challenge_quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rivers_style.easy_challenge_quiz
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
